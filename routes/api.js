41e65a933cc95cadbd3a82122e261ea034ddb2ca
Nilesh Shinde <nilesh.0604@gmail.com>
2016-09-22 11:49:00 +0800

    Initial Commit

---
+++ routes/api.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+var Products = require('../models/products');
+Products.methods(['get', 'put', 'post', 'delete']);
+Products.register(router, '/products');
+
+module.exports = router;
