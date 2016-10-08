6ebef6050a6c17daea794a017ee5ac646c67a5d9
Nilesh Shinde <nilesh.0604@gmail.com>
2016-10-08 15:03:59 +0800

    add, edit, remove routes added

--- models/products.js
+++ models/products.js
@@ -9,9 +9,41 @@ var productSchema = new mongoose.Schema({
     images: [String],
     tags: [String],
     createdOn: { type: Date, default: Date.now },
+	modifiedOn: { type: Date, default: Date.now },
     itemLocation: String,
-    categoryId: Number,
+    category: String,
     addedByUserId: Number
 });
 
-module.exports = restful.model('products', productSchema);
+var Product = module.exports = restful.model('Product', productSchema);
+
+module.exports.getProducts = function (callback, limit){
+	Product.find(callback).limit(limit);
+};
+
+module.exports.getProductById = function (id, callback){
+	Product.findById(id, callback);
+};
+
+module.exports.addProduct = function (product, callback){
+	Product.create(product, callback);
+};
+
+module.exports.updateProduct = function (product, options, callback){
+	var query = {_id: product._id};
+	var update = {
+		name: product.name,
+		description: product.description,
+		price: product.price,
+		tags: product.tags,
+		itemLocation: product.itemLocation,
+		category: product.category,
+		modifiedOn: Date.now()
+	};
+	Product.findOneAndUpdate(query, update, options, callback);
+};
+
+module.exports.removeProduct = function (id, callback){
+	var query = {_id: id};
+	Product.remove(query, callback);
+};
--- server.js
+++ server.js
@@ -1,44 +1,88 @@
 var express = require('express'),
-mongoose = require('mongoose'),
-bodyParser = require('body-parser');
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
 
+Product = require('./models/products');
 
 mongoose.connect('mongodb://ibarteruser:ibarterpass@ds161175.mlab.com:61175/ibarter_db');
 
-var app =  express();
+var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-/*var openshiftConf = {};
-
-openshiftConf.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-openshiftConf.port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
-if (typeof openshiftConf.ipaddress === "undefined") {
-    //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-    //  allows us to run/test the app locally.
-    console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-    openshiftConf.ipaddress = "127.0.0.1";
-};*/
 
 
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "DELETE, GET, HEAD, POST, PUT, OPTIONS, TRACE");
+    next();
 });
 
-app.use('/api', require('./routes/api'));
+//app.use('/api', require('./routes/api'));
+
+app.get('/', function(req, res) {
+    res.send('Please use api/products');
+})
+
+app.get('/api/products', function(req, res) {
+    Product.getProducts(function(err, products){
+      if(err){
+        throw err;
+      }
+      res.send(products);
+    });
+});
+
+app.get('/api/products/:_id', function(req, res) {
+    Product.getProductById(req.params._id, function(err, product){
+      if(err){
+        throw err;
+      }
+      res.send(product);
+    });
+});
+
+app.post('/api/products', function(req, res) {
+    Product.addProduct(req.body, function(err, product){
+      if(err){
+        throw err;
+      }
+      res.send(product);
+    });
+});
+
+app.get('/api/products/:_id', function(req, res) {
+    Product.getProductById(req.params._id, function(err, product){
+        if(err){
+            throw err;
+        }
+        res.send(product);
+    });
+});
+
+app.put('/api/products', function(req, res) {
+    Product.updateProduct(req.body, {}, function(err, product){
+      if(err){
+        throw err;
+      }
+      res.send(product);
+    });
+});
 
 
-/*app.listen(openshiftConf.port, openshiftConf.ipaddress, function() {
-    console.log('%s: Node server started on %s:%d ...',
-    Date(Date.now()), openshiftConf.ipaddress, openshiftConf.port);
-});*/
+app.delete('/api/products/:_id', function(req, res) {
+    Product.removeProduct(req.params._id, function(err, product){
+      if(err){
+        throw err;
+      }
+      res.send(product);
+    });
+});
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
